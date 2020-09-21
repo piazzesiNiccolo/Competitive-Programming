da970361f74e2bd68b96d1f243622ae4effe776d
Niccolò Piazzesi <npiazzesi@gmail.com>
2020-09-21 10:11:16 +0200

    working trapping rain water

--- Lecture_02/Trapping_rain_water/rain_water.cpp
+++ Lecture_02/Trapping_rain_water/rain_water.cpp
@@ -1,32 +1,52 @@
 #include<iostream>
 #include <vector>
+#include <algorithm>
 
+void  total_rain_trapped(std::vector<int> const& vec){
+    std::vector<int> left_max;
+    std::vector<int> right_max;
+    left_max.reserve(vec.size());
+    right_max.reserve(vec.size());
+    int max = 0;
+    int rain = 0;
+    for (auto const& el: vec)
+    {   
+        max = std::max(max, el);
+        left_max.push_back(max);
+    }
+    max = 0;
+    for (auto j = vec.rbegin(); j < vec.rend() ; ++j)
+    {   
+        max = std::max(max,*j);
+        right_max.push_back(max);
+    }
+
+    for (auto i = 0; i < vec.size(); i++)
+    {
+        rain += std::min(left_max.at(i),right_max.at(vec.size() - i -1)) - vec.at(i);
+    }
+    std::cout << rain;
+}
 
 int main(int argc, char const *argv[])
 {
-    std::vector<int> v{7,4,0,9};
-
-    int max= 0;
-    int sum = 0;
-    int n = v.size();
-    for (auto i = 0; i < n; i++)
-    {   
-        
-        if (v[i] >= max)
+    int tests, arr_size;
+    std::vector<int> vec;
+    std::cin >> tests;
+    for (auto i = 0; i < tests; i++)
+    {
+        int n;
+        std::cin >> n;
+        vec.reserve(n);
+        for (auto i = 0; i < n; i++)
         {
-            max = v[i];
+            int x;
+            std::cin >> x;
+            vec.push_back(x);
         }
-        else
-        {
-            sum += max - v[i];
-        }
-        
-        
+        total_rain_trapped(vec);
+        std::cout << std::endl;
+        vec.clear();
     }
-    std::cout << sum << std::endl;
-    
     return 0;
-}
-
-
-
+}   
