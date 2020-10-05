9464f8089d887eed80efcdabcafdb87e50a1400f
niccoloPiazzesi <npiazzesi@gmail.com>
2020-10-05 15:48:45 +0200

    better maxPathsum

--- Lecture_06/Max_path_sum/max_path_sum.cpp
+++ Lecture_06/Max_path_sum/max_path_sum.cpp
@@ -5,21 +5,15 @@ int maxSum(Node* n,int &maxsum){
     if(n == NULL){
         return 0;
     }
-    
-    if (!n->left && !n->right){
-        return n->data;  
-    }  
-    
     if(n->right&&n->left){
         int l = maxSum(n->left,maxsum);
         int r = maxSum(n ->right, maxsum);
         maxsum = std::max(maxsum, l+r+n->data);
         return n->data + max(l,r);
     }
-    else {
-        return (!n->left)? maxSum(n ->right, maxsum) + n->data: 
-                           maxSum(n ->left, maxsum) + n->data; 
-    }    
+    return (!n->left)?  maxSum(n ->right, maxsum) + n->data: 
+                        maxSum(n ->left, maxsum) + n->data; 
+        
 }      
 
 int maxPathSum(Node* root)
